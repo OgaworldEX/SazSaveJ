e2e1d7941f15c5bd2f163c3d48a8409bcc4ba764
Tsuyoshi Ogawa <OgaworldEX@users.noreply.github.com>
2023-08-27 15:24:12 +0900

    Update SazSaveMenuItemsProvider.java
    delete copyright

--- src/main/java/burp/SazSaveMenuItemsProvider.java
+++ src/main/java/burp/SazSaveMenuItemsProvider.java
@@ -1,10 +1,4 @@
 package burp;/*
- * Copyright (c) 2023. PortSwigger Ltd. All rights reserved.
- *
- * This code may be used to extend the functionality of Burp Suite Community Edition
- * and Burp Suite Professional, provided that this usage does not violate the
- * license terms for those products.
- */
 
 import burp.api.montoya.MontoyaApi;
 import burp.api.montoya.core.ToolType;
